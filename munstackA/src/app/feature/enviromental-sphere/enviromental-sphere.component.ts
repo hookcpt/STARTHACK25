5f94d4f5a87311a801a586bceb017f8d0330d116
Glody Figueiredo <glodyfigueiredo@outlook.com>
2025-03-20 17:53:45 +0100

    create enviromental-sphere component

---
+++ munstackA/src/app/feature/enviromental-sphere/enviromental-sphere.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enviromental-sphere',
+  templateUrl: './enviromental-sphere.component.html',
+  styleUrls: ['./enviromental-sphere.component.scss'],
+})
+export class EnviromentalSphereComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
