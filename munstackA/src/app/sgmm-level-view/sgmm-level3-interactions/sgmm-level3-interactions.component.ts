966cbd2bcda04af4b31fed08c8acb1c5c2855269
Piet Vinnbruck <pvinnbru@uni-muenster.de>
2025-03-21 01:50:48 +0100

    Added stuff

---
+++ munstackA/src/app/sgmm-level-view/sgmm-level3-interactions/sgmm-level3-interactions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sgmm-level3-interactions',
+  templateUrl: './sgmm-level3-interactions.component.html',
+  styleUrls: ['./sgmm-level3-interactions.component.scss'],
+  standalone: true,
+})
+export class SgmmLevel3InteractionsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
